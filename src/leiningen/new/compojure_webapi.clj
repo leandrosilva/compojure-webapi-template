e7f24ca29a8532646fc288af4a71519c4e5f39ab
Leandro Silva <leandrodoze@gmail.com>
2012-06-03 23:43:41 -0300

    fixing

--- src/leiningen/new/compojure_webapi.clj
+++ src/leiningen/new/compojure_webapi.clj
@@ -1,17 +1,20 @@
 (ns leiningen.new.compojure-webapi
   (:use [leiningen.new.templates :only [renderer sanitize year ->files]]))
 
-(defn compojure
+(def render (renderer "compojure_webapi"))
+
+(defn compojure_webapi
   "Create a new Compojure project to build a Web API"
   [name]
   (let [data {:name name
               :sanitized (sanitize name)
-              :year (year)}
-        render #((renderer "compojure_webapi") % data)]
+              :year (year)}]
+    (println
+      (str "Generating a project called " name "."))
     (->files data
-             [".gitignore"                       (render "gitignore")]
-             ["project.clj"                      (render "project.clj")]
-             ["README.md"                        (render "README.md")]
-             ["resources/public/index.html"      (render "resources/public/index.html")]
-             ["src/{{sanitized}}/core.clj"       (render "src/webapi/core.clj")]
-             ["test/{{sanitized}}/test/core.clj" (render "test/webapi/test/core.clj")])))
+             [".gitignore"                       (render "gitignore" data)]
+             ["project.clj"                      (render "project.clj" data)]
+             ["README.md"                        (render "README.md" data)]
+             ["resources/public/index.html"      (render "resources/public/index.html" data)]
+             ["src/{{sanitized}}/core.clj"       (render "src/webapi/core.clj" data)]
+             ["test/{{sanitized}}/test/core.clj" (render "test/webapi/test/core.clj" data)])))
